c89a1eee43836715e24659d67a30c6501b518530
SampleUser0001 <ittimfn+github@gmail.com>
2021-09-24 18:11:47 +0900

    tmp

--- app/src/app.py
+++ app/src/app.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from logging import getLogger, config, StreamHandler, DEBUG
 import os
-
 import sys
-sys.path.append('./')
 from logutil import LogUtil
 from importenv import ImportEnvKeyEnum
 import importenv as setting
+import json
+import glob
 
 PYTHON_APP_HOME = os.getenv('PYTHON_APP_HOME')
 logger = getLogger(__name__)
@@ -18,6 +18,33 @@ logger.setLevel(DEBUG)
 logger.addHandler(handler)
 logger.propagate = False
 
+def get_keys(dic):
+  return_list = []
+
+  for key in dic.keys():
+    item = dic[key]
+    return_list.append(key)
+    if isinstance(item, dict):
+      return_list.append(get_keys(item))
+
+  return return_list
+
+def merge(dic, origin):
+  logger.debug('before origin keys : {}'.format(get_keys(origin)))
+  logger.debug('before    dic keys : {}'.format(get_keys(dic)))
+  origin.update(dic)
+  logger.debug('after  origin keys : {}'.format(get_keys(origin)))
+
+  for key in dic.keys():
+    # originを戻す想定。originになくて、dicにあるものをマージしたい。
+    # 型判定する。dictを持っていた場合再帰呼び出しする。
+    dic_item = dic[key]
+    if isinstance(dic_item, dict):
+      origin[key] = merge(dic_item, origin[key])
+
+  logger.debug('return origin keys : {}'.format(get_keys(origin)))
+  return origin
+
 if __name__ == '__main__':
   # .envの取得
   # setting.ENV_DIC[ImportEnvKeyEnum.importenvに書いた値.value]
@@ -26,6 +53,15 @@ if __name__ == '__main__':
   # args = sys.argv
   # args[0]はpythonのファイル名。
   # 実際の引数はargs[1]から。
-  
-  print('Hello Python on Docker!!')
-  logger.info('This is logger message!!')
+
+  merged_dict = {}
+  key_list = []
+  for file in [p for p in glob.glob(PYTHON_APP_HOME + '/input/*' , recursive=True) if os.path.isfile(p) and os.path.splitext(p)[1][1:] != 'gitkeep' ]:
+    logger.info(file)
+
+    with open(file, mode='r') as f:
+      merged_dict = merge(json.load(f), merged_dict)
+
+  logger.info(merged_dict)
+  with open(PYTHON_APP_HOME + '/output/result.json', mode='w', encoding='utf8') as f:
+    f.write(json.dumps(merged_dict))
